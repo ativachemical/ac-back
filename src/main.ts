31019a3934fcab0f5bfbe1f8c8e0a8eb21709fa5
febreno <felipesugisawa1@gmail.com>
2024-08-09 09:49:22 -0300

    new cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaClientExceptionFilter } from './common/filters/prisma-exception.filter';
 import { resolve } from 'path';
 import { writeFileSync, mkdirSync, existsSync } from 'fs';
-import { Request } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,36 +11,25 @@ async function bootstrap() {
   // Configuração CORS dinâmica para múltiplas origens
   app.enableCors({
     origin: (origin, callback) => {
-      // Allow undefined origins (e.g., Swagger UI, server-to-server, Postman)
-      console.log('Request Origin:', origin);
-      if (!origin) {
-        return callback(null, true);
+      // Permite qualquer origem para a rota do Swagger
+      if (origin === undefined || origin.includes('/swagger')) {
+        callback(null, true);
+      } else {
+        const allowedOrigins = [
+          'http://localhost:3000',
+          'http://localhost:3001',
+          'https://www.ativachemical.com',
+          'https://ativachemical.com',
+          'https://ac-front.vercel.app',
+          'https://ac-front-git-development-ativachemicals-projects.vercel.app',
+          'ac-front-ativachemicals-projects.vercel.app',
+        ];
+        if (allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
       }
-
-      // List of allowed origins
-      const allowedOrigins = [
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'https://www.ativachemical.com',
-        'https://ativachemical.com',
-        'https://ac-front.vercel.app',
-        'https://ac-front-git-development-ativachemicals-projects.vercel.app',
-        'https://ac-front-ativachemicals-projects.vercel.app',
-      ];
-
-      //Permitir acesso à rota /swagger
-      if (origin.includes('/swagger')) {
-        return callback(null, true);
-      }
-      // Allow if origin is in the allowedOrigins array
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      console.error('CORS Blocked Origin:', origin); 
-
-      // If none of the above, deny with CORS error
-      return callback(new Error('Not allowed by CORS'));
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
@@ -58,15 +46,6 @@ async function bootstrap() {
     ],
     credentials: true,
   });
-
-  // Middleware para bloquear acesso fora da rota /swagger
-  // app.use((req: Request, res, next) => {
-  //   if (req.originalUrl.startsWith('/swagger')) {
-  //     return next();
-  //   } else {
-  //     return res.status(403).json({ message: 'Access Forbidden' });
-  //   }
-  // });
 
   const config = new DocumentBuilder()
     .setTitle('NestJS API')
